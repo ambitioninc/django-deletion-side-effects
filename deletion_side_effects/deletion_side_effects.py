035147d182d35e8ec131c3d0a12bd37188aa4e01
Wes Kendall <wes.kendall@ambition.com>
2015-05-03 17:19:54 +0000

    changed side effect interface and updated tests

--- deletion_side_effects/deletion_side_effects.py
+++ deletion_side_effects/deletion_side_effects.py
@@ -1,12 +1,11 @@
 from collections import defaultdict
-from itertools import chain
 
 
 # The global variable that holds all side effects that have been registered
 _DELETION_SIDE_EFFECTS = defaultdict(set)
 
 
-def register_deletion_side_effects(sender):
+def register_deletion_side_effects():
     """
     Registers the function as a deletion side effect handler. This function
     should return a list of side effects that occur when a provided object is about to
@@ -20,34 +19,39 @@ def register_deletion_side_effects(sender):
     def _register_deletion_side_effects_wrapper(deletion_side_effects_handler):
         if not issubclass(deletion_side_effects_handler, BaseDeletionSideEffects):
             raise ValueError('Deletion side effects handler must inherit BaseDelectionSideEffects')
+        elif deletion_side_effects_handler.deleted_obj_class is None:
+            raise ValueError('Deletion side effects handler must define a deleted_obj_class variable')
 
-        _DELETION_SIDE_EFFECTS[sender].add(deletion_side_effects_handler)
+        _DELETION_SIDE_EFFECTS[deletion_side_effects_handler.deleted_obj_class].add(deletion_side_effects_handler)
         return deletion_side_effects_handler
 
     return _register_deletion_side_effects_wrapper
 
 
-def _recursive_gather_delection_side_effects(obj_class, objs, gathered_side_effects, gathered_deleted_objs):
-    gathered_deleted_objs.add(objs)
+def _recursive_gather_delection_side_effects(deleted_obj_class, deleted_objs, all_side_effects, all_deleted_objs):
+    # The objects being passed in are deleted, so add them to the set of all deleted objects
+    all_deleted_objs.add(*deleted_objs)
 
-    side_effect_objs, deleted_objs = zip(chain(*[
-        deletion_side_effects(obj_class).get_side_effects(objs)
-        for deletion_side_effects in _DELETION_SIDE_EFFECTS[obj_class]
-    ]))
+    # Pass the deleted objects through the registered side effect classes for the deleted object class
+    all_cascade_deleted_objs = defaultdict(set)
+    for side_effects_class in _DELETION_SIDE_EFFECTS[deleted_obj_class]:
+        side_effect_objs, cascade_deleted_objs = side_effects_class().get_side_effects(deleted_objs)
 
-    gathered_side_effects[_DELETION_SIDE_EFFECTS[obj_class]].add(*side_effect_objs)
+        # Add the side effects from this round of recursion to the set of all side effects for that side effect
+        # class so far
+        if side_effect_objs:
+            all_side_effects[side_effects_class].add(*side_effect_objs)
 
-    # Organize all deleted objects into a mapping of their classes. Only populate deleted objects
-    # here that have not been deleted before in order to avoid circular deletions.
-    mapped_deleted_objs = defaultdict(set)
-    for obj in deleted_objs & gathered_deleted_objs:
-        mapped_deleted_objs[obj.__class__].add(obj)
+        # Add to the set of all cascade deleted objects this round. This is keyed on object type
+        for cascade_deleted_obj in [o for o in cascade_deleted_objs if o not in all_deleted_objs]:
+            all_cascade_deleted_objs[cascade_deleted_obj.__class__].add(cascade_deleted_obj)
 
-    for deleted_class, deleted_objs in mapped_deleted_objs.items():
+    for deleted_class, deleted_objs in all_cascade_deleted_objs.items():
+        # Gather cascading deletion side effects
         _recursive_gather_delection_side_effects(
-            deleted_class, deleted_objs, gathered_side_effects, gathered_deleted_objs)
+            deleted_class, deleted_objs, all_side_effects, all_deleted_objs)
 
-    return gathered_side_effects
+    return all_side_effects
 
 
 def gather_deletion_side_effects(obj_class, objs):
@@ -60,8 +64,8 @@ def gather_deletion_side_effects(obj_class, objs):
     # Render the side effect messages and reorganize the output
     return [
         {
-            'msg': side_effect.get_side_effect_message(side_effect_objs),
-            'side_effect_objs': side_effect_objs,
+            'msg': side_effect().get_side_effect_message(list(side_effect_objs)),
+            'side_effect_objs': list(side_effect_objs),
         }
         for side_effect, side_effect_objs in gathered_side_effects.items()
     ]
@@ -71,8 +75,7 @@ class BaseDeletionSideEffects(object):
     """
     Provides the interface for a user to make a delection side effects class.
     """
-    def __init__(self, deleted_obj_class):
-        self.deleted_obj_class = deleted_obj_class
+    deleted_obj_class = None
 
     def get_side_effects(self, deleted_objects):
         """
--- deletion_side_effects/tests/test_deletion_side_effects.py
+++ deletion_side_effects/tests/test_deletion_side_effects.py
@@ -1,18 +1,47 @@
 from django.contrib.contenttypes.models import ContentType
 from django.test import SimpleTestCase
+from mock import Mock
 
 from deletion_side_effects.deletion_side_effects import (
     BaseDeletionSideEffects, register_deletion_side_effects,
-    _DELETION_SIDE_EFFECTS
+    _DELETION_SIDE_EFFECTS, gather_deletion_side_effects
 )
 
 
-class MyDeletionSideEffects(BaseDeletionSideEffects):
-    pass
+class TestGatherDeletionSideEffects(SimpleTestCase):
+    def setUp(self):
+        _DELETION_SIDE_EFFECTS.clear()
 
+    def test_no_side_effects(self):
+        class MyDeletionSideEffects(BaseDeletionSideEffects):
+            deleted_obj_class = ContentType
 
-class MyOtherDeletionSideEffects(BaseDeletionSideEffects):
-    pass
+        register_deletion_side_effects()(MyDeletionSideEffects)
+
+        ct = ContentType(id=1)
+        side_effects = gather_deletion_side_effects(ContentType, [ct])
+        self.assertEquals(side_effects, [])
+
+    def test_one_side_effect(self):
+        side_effect_obj = Mock(value='hi')
+
+        class MyDeletionSideEffects(BaseDeletionSideEffects):
+            deleted_obj_class = ContentType
+
+            def get_side_effects(self, deleted_objs):
+                return [side_effect_obj], []
+
+            def get_side_effect_message(self, side_effect_objs):
+                return '{0} objs deleted, first value {1}'.format(len(side_effect_objs), side_effect_objs[0].value)
+
+        register_deletion_side_effects()(MyDeletionSideEffects)
+
+        ct = ContentType(id=1)
+        side_effects = gather_deletion_side_effects(ContentType, [ct])
+        self.assertEquals(side_effects, [{
+            'msg': '1 objs deleted, first value hi',
+            'side_effect_objs': [side_effect_obj],
+        }])
 
 
 class TestRegisterDelectionSideEffects(SimpleTestCase):
@@ -20,25 +49,37 @@ class TestRegisterDelectionSideEffects(SimpleTestCase):
         _DELETION_SIDE_EFFECTS.clear()
 
     def test_register_delection_side_effects_single(self):
-        register_deletion_side_effects(ContentType)(MyDeletionSideEffects)
+        class MyDeletionSideEffects(BaseDeletionSideEffects):
+            deleted_obj_class = ContentType
+
+        register_deletion_side_effects()(MyDeletionSideEffects)
         self.assertEquals(_DELETION_SIDE_EFFECTS, {
             ContentType: set([MyDeletionSideEffects])
         })
 
     def test_register_delection_side_effects_duplicates(self):
-        register_deletion_side_effects(ContentType)(MyDeletionSideEffects)
-        register_deletion_side_effects(ContentType)(MyDeletionSideEffects)
+        class MyDeletionSideEffects(BaseDeletionSideEffects):
+            deleted_obj_class = ContentType
+
+        register_deletion_side_effects()(MyDeletionSideEffects)
+        register_deletion_side_effects()(MyDeletionSideEffects)
         self.assertEquals(_DELETION_SIDE_EFFECTS, {
             ContentType: set([MyDeletionSideEffects])
         })
 
     def test_register_delection_side_effects_multiple(self):
-        register_deletion_side_effects(ContentType)(MyDeletionSideEffects)
-        register_deletion_side_effects(ContentType)(MyOtherDeletionSideEffects)
+        class MyDeletionSideEffects(BaseDeletionSideEffects):
+            deleted_obj_class = ContentType
+
+        class MyOtherDeletionSideEffects(BaseDeletionSideEffects):
+            deleted_obj_class = ContentType
+
+        register_deletion_side_effects()(MyDeletionSideEffects)
+        register_deletion_side_effects()(MyOtherDeletionSideEffects)
         self.assertEquals(_DELETION_SIDE_EFFECTS, {
             ContentType: set([MyDeletionSideEffects, MyOtherDeletionSideEffects])
         })
 
     def test_register_delection_invalid_side_effects_single(self):
         with self.assertRaises(ValueError):
-            register_deletion_side_effects(ContentType)(object)
+            register_deletion_side_effects()(object)
