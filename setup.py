72fa3b2477b9ac02e30050fb486fbac2ae13bfd5
Wes Okes <somewes@gmail.com>
2023-01-24 14:58:32 -0500

    github actions

--- setup.py
+++ setup.py
@@ -17,6 +17,14 @@ def get_version():
         raise RuntimeError('Unable to find version string in {0}.'.format(VERSION_FILE))
 
 
+def get_lines(file_path):
+    return open(file_path, 'r').read().split('\n')
+
+
+install_requires = get_lines('requirements/requirements.txt')
+tests_require = get_lines('requirements/requirements-testing.txt')
+
+
 setup(
     name='django-deletion-side-effects',
     version=get_version(),
@@ -29,26 +37,22 @@ setup(
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 3.2',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
     ],
     license='MIT',
-    install_requires=[
-        'Django>=2.0',
-    ],
-    tests_require=[
-        'psycopg2',
-        'django-nose',
-        'django-dynamic-fixture',
-        'mock>=1.0.1',
-        'coverage>=3.7.1',
-    ],
+    install_requires=install_requires,
+    tests_require=tests_require,
     test_suite='run_tests.run_tests',
     include_package_data=True,
     zip_safe=False,
